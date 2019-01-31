dc0c4ac43e72d63f552adc047263d1f42c16c9de
kyrem1 <goingpiano@gmail.com>
2019-01-30 19:32:16 -0800

    Added Typed.js Implementation

--- tacky.io/js/rainbow.js
+++ tacky.io/js/rainbow.js
@@ -1,16 +1,15 @@
-
+//Implementation of rainbow text using jQuery library
 
 let hue = 80;
 let k = document.querySelector('.middleTitle');
 function loop() {
 
         hue += 1;
-        hue = hue % 255;
+        hue = hue % 360;
 
         let color = "hsl(" + hue + ", " + "55%, 70%)"
         k.style.color = color;
         requestAnimationFrame(loop);
-
 }
 
 loop();
---
+++ tacky.io/js/typed-implement.js
@@ -0,0 +1,10 @@
+//Implementation of typed.js library
+
+
+var typed = new Typed("#typed", {
+    strings: ["My name is James Harbour","My name is tACK(y)","My name is kyrem1"],
+    typeSpeed: 30,
+    smartBackspace: true,
+    backDelay: 500,
+    loop: true
+});
