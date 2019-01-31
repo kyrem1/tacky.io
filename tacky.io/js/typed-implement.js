70d0cf7a3b6ffc7d161b61cea0e5bacbde11fce4
kyrem1 <goingpiano@gmail.com>
2019-01-31 15:08:50 -0800

    Added Padding and Preliminary Boxes

--- tacky.io/js/typed-implement.js
+++ tacky.io/js/typed-implement.js
@@ -1,8 +1,8 @@
 //Implementation of typed.js library
 
 
-var typed = new Typed("#typed", {
-    strings: ["My name is James Harbour","My name is tACK(y)","My name is kyrem1"],
+var typed1 = new Typed("#typed1", {
+    strings: ["My name is James Harbour","My name is tACK(y)^750","My name is kyrem1^750"],
     typeSpeed: 30,
     smartBackspace: true,
     backDelay: 500,
