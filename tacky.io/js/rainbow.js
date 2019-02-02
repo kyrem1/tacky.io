6f5f36370a43d0da8a492a3530ae5e796a9526b9
kyrem1 <goingpiano@gmail.com>
2019-02-01 16:05:01 -0800

    Added First Links and More Boxes

--- tacky.io/js/rainbow.js
+++ tacky.io/js/rainbow.js
@@ -3,13 +3,10 @@
 let hue = 80;
 let k = document.querySelector('.middleTitle');
 function loop() {
-
         hue += 1;
         hue = hue % 360;
-
         let color = "hsl(" + hue + ", " + "55%, 70%)"
         k.style.color = color;
         requestAnimationFrame(loop);
 }
-
 loop();
--- tacky.io/js/typed-implement.js
+++ tacky.io/js/typed-implement.js
@@ -1,10 +1,16 @@
 //Implementation of typed.js library
 
 
-var typed1 = new Typed("#typed1", {
+var typedName = new Typed("#typedName", {
     strings: ["My name is James Harbour","My name is tACK(y)^750","My name is kyrem1^750"],
     typeSpeed: 30,
     smartBackspace: true,
     backDelay: 500,
     loop: true
+});
+
+var typedLink1 = new Typed("#typedLink1", {
+    strings: ["<a id=\"link1\" href=\"https://github.com/kyrem1/\">Github</a>^300\n\n<a id=\"link2\" href=\"https://steamcommunity.com/id/hewillendyou\">Steam</a>"],
+    typeSpeed: 30,
+    startDelay: 1000
 });
