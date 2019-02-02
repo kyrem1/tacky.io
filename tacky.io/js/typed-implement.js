27c0cdda69d2c1bf7180bc38589d9e4bac141c85
kyrem1 <goingpiano@gmail.com>
2019-02-01 16:27:15 -0800

    Added Brilliant.org profile and adjusted height of links box

--- tacky.io/js/typed-implement.js
+++ tacky.io/js/typed-implement.js
@@ -10,7 +10,7 @@ var typedName = new Typed("#typedName", {
 });
 
 var typedLink1 = new Typed("#typedLink1", {
-    strings: ["<a id=\"link1\" href=\"https://github.com/kyrem1/\">Github</a>^700\n\n<a id=\"link2\" href=\"https://steamcommunity.com/id/hewillendyou\">Steam</a>"],
+    strings: ["<a id=\"link1\" href=\"https://github.com/kyrem1/\" target=\"_blank\">Github</a>^700\n\n<a id=\"link2\" href=\"https://steamcommunity.com/id/hewillendyou\" target=\"_blank\">Steam</a>^700\n\n<a id=\"link3\" href=\"https://brilliant.org/profile/james-z8137s/\" target=\"_blank\">Brilliant</a>"],
     typeSpeed: 50,
     startDelay: 1000
 });
