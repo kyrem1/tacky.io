7a24cbe58f50f68eba00d6d1a31e8649b71feb71
kyrem1 <goingpiano@gmail.com>
2019-07-17 18:00:45 -0500

    Added Linkedin

--- tacky.io/js/typed-implement.js
+++ tacky.io/js/typed-implement.js
@@ -10,7 +10,7 @@ var typedName = new Typed("#typedName", {
 });
 
 var typedLink1 = new Typed("#typedLink1", {
-    strings: ["<a id=\"link1\" href=\"https://github.com/kyrem1/\" target=\"_blank\">Github</a>^700\n\n<a id=\"link2\" href=\"https://steamcommunity.com/id/hewillendyou\" target=\"_blank\">Steam</a>^700\n\n<a id=\"link3\" href=\"https://brilliant.org/profile/james-z8137s/\" target=\"_blank\">Brilliant</a>^700\n\n<a id=\"link4\" href=\"https://open.spotify.com/user/goingpiano?si=NCOrZR9kTO2UqO3sqoz_bw\" target=\"_blank\">Spotify</a>^700\n\n<a id=\"link5\" href=\"https://www.youtube.com/channel/UC5duwVl53jA7VSL5O-N4Vuw\" target=\"_blank\">Youtube</a>"],
+    strings: ["<a id=\"link1\" href=\"https://github.com/kyrem1/\" target=\"_blank\">Github</a>^700\n\n<a id=\"link2\" href=\"https://steamcommunity.com/id/hewillendyou\" target=\"_blank\">Steam</a>^700\n\n<a id=\"link3\" href=\"https://brilliant.org/profile/james-z8137s/\" target=\"_blank\">Brilliant</a>^700\n\n<a id=\"link4\" href=\"https://open.spotify.com/user/goingpiano?si=NCOrZR9kTO2UqO3sqoz_bw\" target=\"_blank\">Spotify</a>^700\n\n<a id=\"link5\" href=\"https://www.youtube.com/channel/UC5duwVl53jA7VSL5O-N4Vuw\" target=\"_blank\">Youtube</a>^700\n\n<a id=\"link6\" href=\"https://www.linkedin.com/in/james-h-17015b18b/\" target=\"_blank\">Linkedin</a>"],
     typeSpeed: 50,
     startDelay: 1000
 });
